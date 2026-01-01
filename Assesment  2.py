d1194bd7f4213f221b0afbc4f90135ce4024f176
believerS07 <sujanphombo1233@gmail.com>
2026-01-01 21:46:47 +0545

    Assesment 2.py

--- Assesment  2.py
+++ Assesment  2.py
@@ -103,21 +103,8 @@ class CustomCSV():
         with open(self.filename,"w") as file:
             file.write(",".join(headers)+"\n")
 
-csv = CustomCSV("students.csv")
-headers = csv.get_headers()
-rows = csv.get_rows()
 
-# Delete a line
-csv.delete_row(2)
 
-# Replace a line
-csv.replace_row(1, ["2", "Robert", "robert@example.com"])
-
-# Create a copy
-csv.copy_file("students_backup.csv")
-
-# Clear the file
-csv.clear_file()
 
 
 
